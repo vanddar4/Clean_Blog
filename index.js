00e938fbe726c8a2bede7a2fb53f391eaca2097d
vanddar4 <vanddar4@protonmail.com>
2020-11-05 00:11:36 -0500

    package and public files

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const http = require('http');
-const server = http.createServer((req,res) =>{
-  if(req.url === '/about')
-    res.end('The about page');
-  else if(req.url === '/contact')
-    res.end('The contact page');
-  else if(req.url === '/')
-    res.end('The home page');
-  else {
-    res.writeHead(404)
-    res.end('page not found')
-  }
+const fs = require('fs')
+const homePage = fs.readFileSync('index.html')
+const aboutPage = fs.readFileSync('about.html')
+const contactPage = fs.readFileSync('contact.html')
+const notFoundPage = fs.readFileSync('notfound.html')
+const express = require('express')
+const path = require('path')
+const app = express()
+app.use(express.static('public'))
+
+app.listen(3333,()=>{
+  console.log("App listening on port 3333")
 })
 
-server.listen(3333)
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'index.html'))
+})
+app.get('/contact',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'contact.html'))
+})
+app.get('/about',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'about.html'))
+})
+// const server = http.createServer((req,res) =>{
+//   if(req.url === '/about')
+//     res.end(aboutPage);
+//   else if(req.url === '/contact')
+//     res.end(contactPage);
+//   else if(req.url === '/')
+//     res.end(homePage);
+//   else {
+//     res.writeHead(404)
+//     res.end(notFoundPage)
+//   }
+// })
+
+// server.listen(3333)
