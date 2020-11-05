60ab8963f7bc93a274f25656ee22d0b7c28d808f
vanddar4 <vanddar4@protonmail.com>
2020-11-05 12:05:25 -0500

    routing and templating

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
-const http = require('http');
-const fs = require('fs')
-const homePage = fs.readFileSync('index.html')
-const aboutPage = fs.readFileSync('about.html')
-const contactPage = fs.readFileSync('contact.html')
-const notFoundPage = fs.readFileSync('notfound.html')
+// const http = require('http');
+// const fs = require('fs')
+// const homePage = fs.readFileSync('index.html')
+// const aboutPage = fs.readFileSync('about.html')
+// const contactPage = fs.readFileSync('contact.html')
+// const notFoundPage = fs.readFileSync('notfound.html')
 const express = require('express')
 const path = require('path')
-const app = express()
+const app = new express()
 app.use(express.static('public'))
+const ejs = require('ejs')
+app.set('view engine','ejs')
 
 app.listen(3333,()=>{
   console.log("App listening on port 3333")
 })
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'index.html'))
-})
-app.get('/contact',(req,res)=>{
-  res.sendFile(path.resolve(__dirname,'contact.html'))
+  //res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+  res.render('index')
 })
 app.get('/about',(req,res)=>{
-  res.sendFile(path.resolve(__dirname,'about.html'))
+  // res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+  res.render('about')
+
 })
+app.get('/post',(req,res)=>{
+  // res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+  res.render('post')
+
+})
+app.get('/contact',(req,res)=>{
+  // res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+  res.render('contact')
+
+})
+
+// app.get('/',(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,'index.html'))
+// })
+// app.get('/contact',(req,res)=>{
+//   res.sendFile(path.resolve(__dirname,'contact.html'))
+// })
+// app.get('/about',(req,res)=>{
+//   res.sendFile(path.resolve(__dirname,'about.html'))
+// })
 // const server = http.createServer((req,res) =>{
 //   if(req.url === '/about')
 //     res.end(aboutPage);
